45fb5e0d100ac0d1dacf8d0d6ca75791f5d106a2
Cameron Diver <cameron.diver94@gmail.com>
2015-04-09 14:26:52 +0100

    Added the idea of a currentFunction variable for generating code, and getting references to values. Also added static helper function to codegen to create stack allocation variables.
    Signed-off-by: Cameron Diver <cameron.diver94@gmail.com>

--- src/codegen/codegen.cpp
+++ src/codegen/codegen.cpp
@@ -4,6 +4,17 @@ using namespace llvm;
 
 std::map<std::string, llvm::Value *> CodeGen::Symbols;
 
+
+CodeGen::CodeGen(AbstractSyntaxTree *tree) {
+	for(unsigned i = 0; i < tree->functions.size(); ++i) {
+		FunctionDefinition *fn = tree->functions[i];
+		currentFn = fn;
+
+		Value *f = fn->codegen();
+		
+	}
+}
+
 Type *CodeGen::getTypeFromBuiltin(BuiltinType t) {
 	switch(t) {
 		case INTEGER:
@@ -17,4 +28,11 @@ Type *CodeGen::getTypeFromBuiltin(BuiltinType t) {
 			return NULL;
 		break;
 	}
+}
+
+AllocaInst *CodeGen::createEntryBlockAlloca(Function *fn, const std::string name, BuiltinType t) {
+
+	IRBuilder<> tempBuilder(fn->getEntryBlock(), fn->getEntryBlock().begin());
+
+	return tempBuilder.CreateAlloca(getTypeFromBuiltin(t), 0, name);
 }
--- src/codegen/codegen.h
+++ src/codegen/codegen.h
@@ -5,15 +5,22 @@
 #include <string>
 
 #include "builtins.h"
+#include "ast.h"
+
+// the current function
+extern FunctionDefinition *currentFn;
 
 class CodeGen {
 
 public:
 	static std::map<std::string, llvm::Value *> Symbols;
-
-
-
 	static llvm::Type *getTypeFromBuiltin(BuiltinType t);
+	// create a allocate instruction for creating variables on the stack
+	static llvm::AllocaInst *
+		createEntryBlockAlloca(llvm::Function *fn, const std::string name);
+
+
+	CodeGen(AbstractSyntaxTree *tree);
 
 };
 
