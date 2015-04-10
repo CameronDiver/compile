a14cd60eeca372a11c2ad1b0b70b016b18ea0569
Cameron Diver <cameron.diver94@gmail.com>
2015-04-10 17:44:45 +0100

    Added function body code generation

--- src/codegen/functionbody.cpp
+++ src/codegen/functionbody.cpp
@@ -1,7 +1,25 @@
 #include "functionbody.h"
 
+#include <cstdlib>
+#include <iostream>
+using namespace llvm;
 
 
-llvm::Value *FunctionBody::codegen() {
-	
+Value *FunctionBody::codegen() {
+	// go through all of the statements and generate code for the function
+	llvm::Value *last = NULL;
+	for(unsigned int i = 0; i < statements.size(); ++i) {
+		if(statements[i] == NULL) {
+			// FIXME: empty function or error?
+		} else {
+			Value *val = statements[i]->codegen();
+
+			if(val == NULL) {
+				std::cout << "An error occured" << std::endl;
+				exit(-1);
+			}
+			last = val;
+		}
+	}
+	return last;
 }
