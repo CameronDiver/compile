e6498c24f6aab36636b8d53459d421fbcf969161
Cameron Diver <cameron.diver94@gmail.com>
2015-04-16 10:26:13 +0100

    Added more complex test which finds primes by doing trial division

---
+++ test/trialdivision.f
@@ -0,0 +1,28 @@
+int mod | int a, int b |
+	while a >= b
+		a = a - b
+	end
+	a
+end
+
+int main ||
+	int prime = 1
+	while prime < 100000
+		int divisor = prime - 1
+		int isprime = 1
+		while divisor > 1
+			if mod(prime, divisor)
+				
+			else
+				isprime = 0
+				divisor = divisor - divisor + 1
+			end
+			divisor -= 1
+		end
+
+		if isprime
+			printNum(prime)
+		end
+		prime += 1
+	end
+end
