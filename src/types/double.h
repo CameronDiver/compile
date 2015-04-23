ade214a91c34625079e9eda2fe6114663283952e
Cameron Diver <cameron.diver94@gmail.com>
2015-04-23 21:43:26 +0100

    Added definitions for required virtual functions in all currently started types.

--- src/types/double.h
+++ src/types/double.h
@@ -10,6 +10,27 @@ class Double : public LanguageType {
  	Double(double val);
 
  	llvm::Type *getLLVMType();
+
+ 	LanguageType *add(LanguageType *operand);
+ 	LanguageType *sub(LanguageType *operand);
+ 	LanguageType *mul(LanguageType *operand);
+ 	LanguageType *div(LanguageType *operand);
+ 	LanguageType *assign(LanguageType *operand);
+ 	LanguageType *lt(LanguageType *operand);
+ 	LanguageType *gt(LanguageType *operand);
+ 	LanguageType *neq(LanguageType *operand);
+ 	LanguageType *mod(LanguageType *operand);
+ 	LanguageType *lte(LanguageType *operand);
+ 	LanguageType *gte(LanguageType *operand);
+ 	LanguageType *comp(LanguageType *operand);
+ 	LanguageType *_and(LanguageType *operand);
+ 	LanguageType *band(LanguageType *operand);
+ 	LanguageType *_not(LanguageType *operand);
+ 	LanguageType *bnot(LanguageType *operand);
+ 	LanguageType *_or(LanguageType *operand);
+ 	LanguageType *bor(LanguageType *operand);
+ 	LanguageType *_xor(LanguageType *operand);
+ 	LanguageType *bxor(LanguageType *operand);
 };
 
 #endif 
