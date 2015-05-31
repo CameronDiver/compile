87cc7b4d1d0c5f2550943040d493406e6dcc7786
Cameron Diver <cameron.diver94@gmail.com>
2015-05-31 22:51:13 +0100

    Added parse tree support for strings and symbols

--- src/parser/parse.h
+++ src/parser/parse.h
@@ -197,4 +197,34 @@ class FunctionCall : public SyntaxTreeNode {
  	}
 };
 
+class Symbol : public SyntaxTreeNode {
+ public:
+ 	std::string symbol;
+ 	Symbol(char *_sym)
+ 	:	SyntaxTreeNode("Symbol"), symbol(_sym) {
+ 	};
+
+ 	std::string getStr() {
+ 		std::stringstream ss;
+ 		ss << "Symbol: " << symbol;
+ 		return ss.str();
+ 	}
+};
+
+class String : public SyntaxTreeNode {
+ public:
+ 	std::string image;
+ 	String(char *_image)
+ 	:	SyntaxTreeNode("String"), image(_image) {
+ 		// TODO: Escaping
+ 	};
+
+
+ 	std::string getStr() {
+ 		std::stringstream ss;
+ 		ss << "String " << image;
+ 		return ss.str();
+ 	}
+};
+
 #endif
