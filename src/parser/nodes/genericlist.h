295cb7d30071d31e561d6a06e3d95ce4ad4bd8dd
Cameron Diver <cameron.diver94@gmail.com>
2015-06-30 21:39:36 +0100

    Changed the GenericList class to derive from SyntaxTreeNode to reduce bugs.

--- src/parser/nodes/genericlist.h
+++ src/parser/nodes/genericlist.h
@@ -1,15 +1,72 @@
 #ifndef PARSE_GENERIC_LIST_H
 #define PARSE_GENERIC_LIST_H
 
-#include "list.h"
+#include <vector>
 
-class GenericList : public NodeList<SyntaxTreeNode> {
+#include "node.h"
+
+// Inherit from SyntaxTreeNode instread of NodeList,
+// as using the base SyntaxTreeNode class as the template
+// for NodeList produces extra checking code which isn't
+// necessary (or helpful)
+// 
+class GenericList : public SyntaxTreeNode {
 public:
 	GenericList(SyntaxTreeNode *child)
-	:	NodeList("List", child) {}
+	:	SyntaxTreeNode("List") {
+		addChild(child);
+	}
 
 	GenericList()
-	:	NodeList("List") {}
+	:	SyntaxTreeNode("List") {}
+
+	const unsigned size() const {
+		return children.size();
+	}
+
+	void addChild(SyntaxTreeNode *child) {
+		children.push_back(child);
+	}
+
+	std::vector<SyntaxTreeNode *> getChildren() const {
+		return children;
+	}
+
+	std::string getStr() {
+		std::stringstream ss;
+		ss << "List:\n\t";
+		for(auto c : children) {
+			ss << c->getStr() << ",\n\t";
+		}
+
+		return ss.str();
+	}
+
+	SyntaxTreeNode *operator[](unsigned idx) {
+		return children[idx];
+	}
+
+	std::vector<SyntaxTreeNode *>::iterator begin() {
+		return children.begin();
+	}
+
+	std::vector<SyntaxTreeNode *>::iterator end() {
+		return children.end();
+	}
+
+	bool isList() {
+		return true;
+	}
+ 
+	void printSubtree(unsigned depth=0) {
+		for(auto c : children) {
+			for(uint i = 0; i < depth; ++i) std::cout << "   ";
+			std::cout << c->getStr() << "\n";
+		}
+	}
+
+private:
+	std::vector<SyntaxTreeNode *> children;
 };
 
 #endif // PARSE_GENERIC_LIST_H
